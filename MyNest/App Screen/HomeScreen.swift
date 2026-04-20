06349b62552bc13beda6e7247c90e35c8c435476
ThanviChada <thanvi.chada@gmail.com>
2026-04-20 10:01:03 -0500

    homescreen version2

--- MyNest/App Screen/HomeScreen.swift
+++ MyNest/App Screen/HomeScreen.swift
@@ -18,38 +18,22 @@ struct HomeScreen: View {
                     .font(Font.custom("Instrument Sans", size: 22).weight(.bold))
                     .foregroundColor(Color(red: 0.17, green: 0.60, blue: 0.80))
                     .offset(x: 138, y: 397.50)
-
-                Rectangle()
-                    .frame(width: 59, height: 1)
-                    .foregroundColor(Color.gray.opacity(0.7))
-                    .offset(x: -34.50, y: 426)
-
-                Rectangle()
-                    .frame(width: 59, height: 1)
-                    .foregroundColor(Color.gray.opacity(0.7))
-                    .offset(x: 107.50, y: 426)
- 
-                Rectangle()
-                    .frame(width: 410, height: 45)
-                    .background(Color(red: 0.79, green: 0.85, blue: 0.72).opacity(0.76))
-                    .overlay(
-                        Rectangle()
-                            .stroke(Color.black, lineWidth: 0.5)
-                    )
-                    .offset(x: 2, y: -418.50)
             }
 
             Group {
+               //order pic
                 Rectangle()
                     .frame(width: 76, height: 76)
                     .background(Color(red: 0.50, green: 0.23, blue: 0.27).opacity(0.50))
                     .offset(x: -142, y: 207)
-
+                
+                //discussion pic
                 Rectangle()
                     .frame(width: 102.55, height: 92.68)
                     .background(Color(red: 0.50, green: 0.23, blue: 0.27).opacity(0.50))
                     .offset(x: 129.27, y: 209.34)
-
+                
+                //donate pic
                 Rectangle()
                     .frame(width: 95, height: 79)
                     .background(Color(red: 0.50, green: 0.23, blue: 0.27).opacity(0.50))
@@ -59,7 +43,7 @@ struct HomeScreen: View {
                     .font(Font.custom("Abhaya Libre Medium", size: 20))
                     .offset(x: -136, y: 281.50)
 
-                Text("Discuss")
+                Text("Discussion Board")
                     .font(Font.custom("Abhaya Libre Medium", size: 20))
                     .offset(x: 137.50, y: 281.50)
 
@@ -70,12 +54,14 @@ struct HomeScreen: View {
                 Ellipse()
                     .frame(width: 330, height: 315)
                     .foregroundColor(Color(.systemGray6))
-                    .offset(x: -5, y: -177.50)
+                    .offset(x: 1, y: -190)
 
-                Rectangle()
-                    .frame(width: 234, height: 211)
-                    .background(Color(red: 0.50, green: 0.23, blue: 0.27).opacity(0.50))
-                    .offset(x: 0, y: -181.50)
+                //logo pic
+                Image("myNest_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 345)
+                    .offset(x: -7, y: -190)
 
                 Text("Hello, (name)!")
                     .font(Font.custom("Kumbh Sans", size: 35).weight(.semibold))
